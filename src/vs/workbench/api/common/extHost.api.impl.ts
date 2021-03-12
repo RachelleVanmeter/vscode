b38fe7782e74c73d84ef3ee078973d62bed49cb6
Rob Lourens <roblourens@gmail.com>
2021-03-11 17:23:59 -0800

    Implement NotebookCellOutputEdit

--- src/vs/workbench/api/common/extHost.api.impl.ts
+++ src/vs/workbench/api/common/extHost.api.impl.ts
@@ -1240,7 +1240,6 @@ export function createApiFactoryAndRegisterActors(accessor: ServicesAccessor): I
 			NotebookCellMetadata: extHostTypes.NotebookCellMetadata,
 			NotebookCellData: extHostTypes.NotebookCellData,
 			NotebookData: extHostTypes.NotebookData,
-			NotebookRunState: extHostTypes.NotebookRunState,
 			NotebookCellStatusBarAlignment: extHostTypes.NotebookCellStatusBarAlignment,
 			NotebookEditorRevealType: extHostTypes.NotebookEditorRevealType,
 			NotebookCellOutput: extHostTypes.NotebookCellOutput,
